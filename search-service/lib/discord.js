73f0a77c9003b9174e6fc8697f098aaf8faf7302
Andrew Haas <ahaas@micds.org>
2024-10-19 13:00:11 -0500

    rename "app" to "search-service"

--- search-service/lib/discord.js
+++ search-service/lib/discord.js
@@ -1,7 +1,7 @@
 if(process.env.DISCORD_WEBHOOK === undefined) {
     console.log("WARNING WEBHOOK NOT DEFINED");
 } else {
-    const { Webhook } = require('discord-webhook-node');
+    const { Webhook } = require('../node_modules/discord-webhook-node');
     const hook = new Webhook(process.env.DISCORD_WEBHOOK);
     
     hook.setUsername('MapsNotIncluded API');
