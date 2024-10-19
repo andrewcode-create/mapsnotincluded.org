8b403645d496bb8557a340e351acf6e91684ffce
Andrew Haas <ahaas@micds.org>
2024-10-19 12:19:32 -0500

    updated exports in index.js

--- app/models/index.js
+++ app/models/index.js
@@ -21,23 +21,9 @@ Cluster.hasMany(Asteroid, {
     foreignKey: 'coordinate'
 });
 
-/*
-Asteriod.hasMany(Geyser, {
-    foreignKey: 'coordinate'
-});
-*/
-
-
-// First time using sequelise, this doesn't make much sense to me as the ID is on the correct table but syntactically i would expect a save to have one file?
-/*
-File.hasOne(Save, {
-    foreignKey: 'fileId'
-});
-*/
-
 module.exports = {
-    Geyser,
     Cluster,
+    Dlc,
+    Asteroid,
     TotalGeyserOutput,
-    //File,
 }
