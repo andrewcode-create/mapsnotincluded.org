a9803fe9376012a2028c85b680d414dc4f2bac3e
Andrew Haas <ahaas@micds.org>
2024-10-19 12:22:44 -0500

    Fixed crash when no discord webhook provided

--- app/lib/discord.js
+++ app/lib/discord.js
@@ -1,8 +1,12 @@
-const { Webhook } = require('discord-webhook-node');
-const hook = new Webhook(process.env.DISCORD_WEBHOOK);
-
-hook.setUsername('MapsNotIncluded API');
- 
-hook.send("New Instance!");
-
-module.exports = hook;
+if(process.env.DISCORD_WEBHOOK === undefined) {
+    console.log("WARNING WEBHOOK NOT DEFINED");
+} else {
+    const { Webhook } = require('discord-webhook-node');
+    const hook = new Webhook(process.env.DISCORD_WEBHOOK);
+    
+    hook.setUsername('MapsNotIncluded API');
+     
+    hook.send("New Instance!");
+    
+    module.exports = hook;
+}
