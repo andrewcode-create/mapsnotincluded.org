65bf8c18ce9c31245ad36429e2a6a4dba40f28f5
Andrew Haas <ahaas@micds.org>
2024-10-24 21:18:29 -0500

    fixed bugs + typos

--- search-service/controllers/Upload.js
+++ search-service/controllers/Upload.js
@@ -31,7 +31,7 @@ const uploadSingleJson = async (jsonOld, numtoPrint) => {
             worldTraits: asteroidData.worldTraits
         }));
 
-        const createdAsteroids = await Asteroid.bulkCreate(asteroidBulkData, { transaction });
+        const createdAsteroids = await Asteroid.bulkCreate(asteroidBulkData, { transaction:transaction , individualHooks: true, validate:true});
        
         const totalGeyserOutputBulkData = createdAsteroids.map(asteroid => ({
             clusterId: null,
@@ -55,7 +55,7 @@ const uploadSingleJson = async (jsonOld, numtoPrint) => {
             i++
         });
 
-        gs = await TotalGeyserOutput.bulkCreate(totalGeyserOutputBulkData, { transaction });
+        gs = await TotalGeyserOutput.bulkCreate(totalGeyserOutputBulkData, { transaction:transaction , individualHooks: true, validate:true});
 
         
         await TotalGeyserOutput.create({
